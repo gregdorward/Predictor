2887ee6f2eefca46cb86645deb60ae486e8a74c5
Greg Dorward <greg.dorward@red-badger.com>
2021-02-17 17:26:39 +0000

    another fix

--- src/logic/getScorePredictions.js
+++ src/logic/getScorePredictions.js
@@ -286,7 +286,13 @@ export async function getScorePrediction(day) {
   }
   let predictionArray = [];
   let storedPredictions = await fetch(
-    `${process.env.REACT_APP_EXPRESS_SERVER}${day}Predictions${divider}`
+    `${process.env.REACT_APP_EXPRESS_SERVER}${day}Predictions${divider}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": "true"
+      },
+    }
   );
 
   predictions = [];
