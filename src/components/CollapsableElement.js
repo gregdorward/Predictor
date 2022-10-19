d3421f837f7825aff8ff1e37aafe5b8c358ec3e6
Greg Dorward <greg.dorward@red-badger.com>
2022-10-19 14:20:13 +0100

    collapsable div

--- src/components/CollapsableElement.js
+++ src/components/CollapsableElement.js
@@ -1,52 +1,13 @@
 import React, { Component } from "react";
-import EscapeOutside from "react-escape-outside";
+import Collapsible from "react-collapsible";
 
-class Collapsable extends Component {
-  state = { isOpen: false };
-
-  handleOnClick = () => {
-    this.setState((prevState) => ({
-      isOpen: !prevState.isOpen,
-    }));
-    console.log(this.state.isOpen);
-  };
-
-  render() {
-    return (
-      <div>
-        <div>
-          <button className={this.props.className} onClick={this.handleOnClick}>
-            {this.props.buttonText}
-          </button>
-        </div>
-        <div style={this.props.style}>
-          {/* <div>{this.props.element}</div> */}
-          {!this.state.isOpen && (
-            <EscapeOutside className="Collapsable">
-              <div className="flex-container">
-                <div className={this.props.classNameDiv}>{this.props.text}</div>
-                <div className={this.props.classNameDivTwo}>
-                  {this.props.textTwo}
-                </div>
-              </div>
-            </EscapeOutside>
-          )}
-          {this.state.isOpen && (
-            <EscapeOutside className="Collapsable">
-              <div className="flex-container">
-                <div className={this.props.classNameDiv}>
-                  {this.props.element}
-                </div>
-                <div className={this.props.classNameDivTwo}>
-                  {this.props.elementTwo}
-                </div>
-              </div>
-            </EscapeOutside>
-          )}
-        </div>
-      </div>
-    );
-  }
-}
+const Collapsable = (props) => {
+  return (
+    <Collapsible transitionTime={300} trigger={<button>{props.buttonText}</button>} className="Collapsable">
+      <span className={props.buttonText} style={props.style} key={123}>{props.element}</span>
+      <span key={456}>{props.elementTwo}</span>
+    </Collapsible>
+  );
+};
 
 export default Collapsable;
