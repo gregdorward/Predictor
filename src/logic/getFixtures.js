b76afe523ca307921e7b0e9328ec6e02da303aaa
Greg Dorward <greg.dorward@red-badger.com>
2024-10-21 09:57:02 +0100

    quick fix and version

--- src/logic/getFixtures.js
+++ src/logic/getFixtures.js
@@ -1598,7 +1598,7 @@ export async function generateFixtures(
               onClickEvent={() => getScorePrediction(day)}
               className={"GeneratePredictions"}
             />
-            <div className="Version">Prediction engine v1.1.0 (17/10/24)</div>
+            <div className="Version">Prediction engine v1.1.1 (21/10/24)</div>
             <Collapsable
               buttonText={"Filters"}
               element={
