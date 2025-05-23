bc59b263db8723838c1f5ec4a3cf3387f75cf235
Greg Dorward <greg.dorward@red-badger.com>
2025-05-23 20:45:26 +0100

    key players more leagues

--- src/components/TeamOfTheSeason.js
+++ src/components/TeamOfTheSeason.js
@@ -10,7 +10,41 @@ export const rounds = [
     7: 61644,
     23: 63515,
     242: 70158,
-    34: 61736,
+    34: 61736, 
+    37: 61666, // dutch
+    9: 61412, // belgium
+    40: 69956, // sweden
+    39: 61326, // denmark
+    20: 70174, // Norway
+    45: 62629, // Austria
+    185: 64052, // Greece
+    52: 63814, // Turkey
+    202: 61236, // Poland
+    215: 61658, // Switz
+    170: 61243, // Croatia
+    172: 61716, // Czech
+    192: 69981, // Ireland
+    357: 69619, // Club World Cup
+    54: 62048, // Spanish Secunda
+    53: 63812, // Serie B
+    325: 72034, // Brazil
+    155: 70268, // Argentina
+    44 : 63514, // Bundesliga 2
+    182 : 61737, // Ligue 2
+    36 : 62408, // Scottish Prem
+    206 : 62411, // Scottish Champ
+    207 : 62416, // Scottish league one
+    209 : 62487, // Scottish league two
+    13470 : 72315, // Canada
+    11621 : 61419, // Liga MX
+    173 : 63807, // English National League
+    176 : 63806, // National League North
+    174 :63803, // National League South
+    136 : 64864, // A League
+    410 : 70830, // South korea
+    196 : 69871, // J League
+    955 : 63998, // Saudi Pro League
+    11 : 69427, // World cup EU
   },
 ];
 
