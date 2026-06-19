29adc3ad245bca6b41ad42f775473043cd031daf
Greg Dorward <greg@Gregs-MacBook-Air.local>
2026-06-19 10:30:11 +0100

    styling

--- src/components/BTTSFixtures.js
+++ src/components/BTTSFixtures.js
@@ -142,7 +142,7 @@ export default function BTTSFixtures() {
                     <Box 
                       style={{ 
                         backgroundColor: "var(--accent-color)", 
-                        color: "#fff", 
+                        color: "var(--button-text-color)", 
                         borderRadius: 4, 
                         padding: "2px 6px", 
                         display: "inline-block",
